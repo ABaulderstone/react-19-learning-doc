ddede24a308aa57be170001b3e4509b1d6a5c1f7
ABaulderstone <alex.baulderstone@gmail.com>
2024-05-30 13:03:51 +1000

    adds title and meta tag to homepage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <>
+      {/* Can now include title and meta tags and they will be inserted in the dom */}
+      <title>Home Page</title>
+      <meta
+        name="description"
+        content="This is the home page. SEO stuff could go here"
+      />
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+export default HomePage;
